4ed5e31e8182d62ce790f5ecd7b8d4cf9e7ea1ed
Alfred Alfie <alfred.alfie123@gmail.com>
2023-02-23 15:13:20 +0700

    Upgrade react dependencies

--- src/app/components/Commons/Modals/CubeLoadingModal.tsx
+++ src/app/components/Commons/Modals/CubeLoadingModal.tsx
@@ -1,11 +1,11 @@
-import { Lottie } from "@crello/react-lottie";
+import Lottie from "lottie-react";
 import { generateLottieData } from "src/app/utils/helpers";
 import * as cubeLoadingJson from 'src/assets/lotties/cube-loading.json';
 
 export default function CubeLoadingModal() {
   return (
     <div className="align-center">
-      <Lottie height='130px' config={generateLottieData(cubeLoadingJson)} />
+      <Lottie height='130px' animationData={generateLottieData(cubeLoadingJson)} />
       <div className='mt-4 fw-3'>Now Loading...</div>
     </div>
   )
